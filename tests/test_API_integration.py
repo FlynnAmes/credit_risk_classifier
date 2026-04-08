5decfb60972a9f9156aba870de765ffbdb7f08f3
Flynn <F.ames@pgr.reading.ac.uk>
2026-04-08 15:35:28 +0100

    update integration tests (note address is hardcoded for now)
     and update CI accordingly

--- tests/test_API_integration.py
+++ tests/test_API_integration.py
@@ -4,13 +4,25 @@ import pytest
 import requests
 
 
+# # test that the API is running after setup with docker
+# @pytest.mark.integration
+# def test_API_is_running_with_docker():
+#     assert requests.get('http://127.0.0.1:8000/health').status_code == 200
+
+
+# # test can get a prediction using valid input data when using docker
+# @pytest.mark.integration
+# def test_get_prediction_with_docker(input_data):
+#     assert requests.post('http://127.0.0.1:8000/predict', json=input_data).status_code == 200
+
+
 # test that the API is running after setup with docker
 @pytest.mark.integration
-def test_API_is_running_with_docker():
-    assert requests.get('http://127.0.0.1:8000/health').status_code == 200
+def test_API_is_available_on_lambda():
+    assert requests.get('https://tq1fek3ld3.execute-api.eu-west-2.amazonaws.com/health').status_code == 200
 
 
 # test can get a prediction using valid input data when using docker
 @pytest.mark.integration
-def test_get_prediction_with_docker(input_data):
-    assert requests.post('http://127.0.0.1:8000/predict', json=input_data).status_code == 200
+def test_get_prediction_from_lambda(input_data):
+    assert requests.post('https://tq1fek3ld3.execute-api.eu-west-2.amazonaws.com/predict', json=input_data).status_code == 200
