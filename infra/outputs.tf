de20e26453d8a28367da2e586b306c73cb9ae0c2
Flynn <F.ames@pgr.reading.ac.uk>
2026-04-13 16:32:09 +0100

    another commit to get plan working

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,5 +1,5 @@
 output "api_url" {
-  description = "url of the api"
+  description = "url of the API"
   # will put this into the README automatically later
   value = aws_apigatewayv2_api.api.api_endpoint
 }
