29d88e0ecf710f8550d932dea4b630578d31adb1
Flynn <F.ames@pgr.reading.ac.uk>
2026-04-14 22:52:57 +0100

    remove old redundant infra files

--- infra/s3.tf
+++
@@ -1,8 +0,0 @@
-# specify region for all resources
-provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_s3_bucket" "s3" {
-  bucket = "credit-risk-classifier-tf"
-}
--- infra/vars.tf
+++
@@ -1,8 +0,0 @@
-# to be defined when running terraform apply
-variable "image_uri" {
-    type = string
-}
-
-variable "model_key_name" {
-    type = string
-}
